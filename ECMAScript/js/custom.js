1707a247db345efb8a7a65f5219e068f01cf9e9a
dunky-star <dukygeoffrey39@gmail.com>
2022-11-11 10:43:50 +0300

    ECMAScript Functions

---
+++ ECMAScript/js/custom.js
@@ -0,0 +1,29 @@
+"use strict"; // strict mode to enforce some type safety.
+
+console.log("Hello World of JS");
+
+let r = 5;
+const pi = 3.14;
+
+let a = 3.14*r*r;
+console.log("The area of a circle is: " +a);
+
+// var awesome = prompt("Enter your first name: ");
+
+function AwesomeFunc()
+{
+    console.log("Hello");
+    console.log("and");
+    console.log("bye");
+}
+
+// Function call
+AwesomeFunc();
+
+function Sum(num1, num2)
+{
+    var result = num1 + num2;
+    console.log(result);
+}
+
+
--- ECMAScript/prog.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello World of JS");
-
-let r = 5;
-const pi = 3.14;
-
-let a = 3.14*r*r;
-console.log("The area of a circle is: " +a);

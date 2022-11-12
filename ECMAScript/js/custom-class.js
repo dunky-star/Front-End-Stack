aaacc393e36bf428f6a3092b98111b288e0ebbfe
dunky-star <dukygeoffrey39@gmail.com>
2022-11-12 14:45:11 +0300

    ECMAScript classes and variables

---
+++ ECMAScript/js/custom-class.js
@@ -0,0 +1,13 @@
+"use strict"; // strict mode to enforce some type safety.
+
+// Classes in JavaScript
+
+class Rectangle{
+
+    constructor(x, y){
+        console.log("The position of X is: " +x);
+        console.log("The position of Y is: " +y);
+        console.log("This is a constructor");
+    }
+
+}

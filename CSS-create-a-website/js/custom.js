e77323db0437e82a6ea2177543fb54ceafd03bae
dunky-star <dukygeoffrey39@gmail.com>
2022-11-23 19:50:48 +0300

    JavaScript, do more on your webpage.

--- CSS-create-a-website/js/custom.js
+++ CSS-create-a-website/js/custom.js
@@ -2,15 +2,65 @@
 
 document.write("Hello World of Java ECMAScript.");
 
-// Objects within objects
+// JavaScript Objects within object
 
 var ourFriends = {
     person1: {
         name: "Duncan", 
-        age: 36
+        age: 36,
+        cars: {carMake: "Subaru", carValue: 50000 }
     }, 
     person2: {
         name:"Deus Kanshabe",
         age: 40
-     }
+    },
+    person3: {
+        name: "Anjana",
+        age: 25
+    }
+    
 };
+
+var a =1;
+
+console.log(ourFriends['person' + a ]);
+a++
+console.log(ourFriends['person' + a ]);
+a++
+console.log(ourFriends['person' + a ]);
+
+
+
+// JavaScript array data structure.
+var ourCars = ["GM", "Mitsubishi", "Subaru", "Ford"]; 
+ourCars[1]  = "Toyota";
+console.log(ourCars);
+console.log(ourCars[2]);
+
+
+// JavaScript nested array data structure.
+var ourAnimals = [["Dog", "Cow", "Cat", "Lion"], ["Bark", "Bellow", "Meow", "Roar"]];
+console.log("Our animal in the multi-dimensional array is: " + ourAnimals[0][2]);
+var g = ourAnimals.toString();   // Converting an array to String for data storage.
+console.log(g);
+var h = g.split(','); // Converting String to an array.
+console.log(h);
+var i = ourAnimals.join(',');  // Alternative Conversion of an array to String for data storage.
+
+
+// JavaScript object within Array.
+var myArray = [
+    {
+        name1: "Lawrence",
+        name2: "King",
+        name3: "David"
+    },
+    {
+        name1: "Tina",
+        name2: "Dave",
+        name3: "John"
+
+    }
+]
+
+console.log(myArray[0].name2);

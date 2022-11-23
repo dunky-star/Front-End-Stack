4d12f451d470c8b11d8275d0302a502e5bf76924
dunky-star <dukygeoffrey39@gmail.com>
2022-11-23 18:20:35 +0300

    JavaScript, do more on your webpage.

--- CSS-create-a-website/js/custom.js
+++ CSS-create-a-website/js/custom.js
@@ -1,3 +1,16 @@
 "use strict"; // strict mode to enforce some type safety.
 
 document.write("Hello World of Java ECMAScript.");
+
+// Objects within objects
+
+var ourFriends = {
+    person1: {
+        name: "Duncan", 
+        age: 36
+    }, 
+    person2: {
+        name:"Deus Kanshabe",
+        age: 40
+     }
+};

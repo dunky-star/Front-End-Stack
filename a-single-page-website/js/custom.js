363edd0b7bf2d6bf18b98a7dd7d394d559645627
dunky-star <dukygeoffrey39@gmail.com>
2022-11-28 10:13:37 +0300

    Add active class js for scroll position

--- a-single-page-website/js/custom.js
+++ a-single-page-website/js/custom.js
@@ -1,69 +1,34 @@
 "use strict"; // strict mode to enforce some type safety.
 
-// JavaScript Objects within object
+// JavaScript window scroll
 
-var ourFriends = {
-    person1: {
-        name: "Duncan", 
-        age: 36,
-        cars: {carMake: "Subaru", carValue: 50000 }
-    }, 
-    person2: {
-        name:"Deus Kanshabe",
-        age: 40
-    },
-    person3: {
-        name: "Anjana",
-        age: 25
+var section = document.querySelectorAll('.section');
+var sections = {};
+
+section.forEach(function(e){
+    sections[e.id] = e.offsetTop;
+})
+
+
+window.onscroll = function(){
+    var scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
+    for (var x in sections){
+        if(sections[x] <= scrollPos){
+            document.querySelector('.active').setAttribute('class','');
+            document.querySelector('a[href*='+x+']').setAttribute('class','active');
+        }
     }
-    
-};
-
-var a =1;
-
-while (a<=3){
-    console.log(ourFriends['person' + a ]);
-    a++
 }
 
+/*
+var sections = {};
+section.forEach(function(e){
+    console.log(e.id, e.offsetTop);
+})
 
-// JavaScript array data structure.
-var ourCars = ["GM", "Mitsubishi", "Subaru", "Ford"]; 
-ourCars[1]  = "Toyota";
-console.log(ourCars);
-console.log(ourCars[2]);
+sections[e.id] = e.offsetTop;
 
-
-// JavaScript nested array data structure.
-var ourAnimals = [["Dog", "Cow", "Cat", "Lion"], ["Bark", "Bellow", "Meow", "Roar"]];
-delete ourAnimals[0][3]  // Deleting an element from array.
-ourAnimals[0][3] = "Lioness";  // Adding an element to an array.
-console.log("Our animal in the multi-dimensional array is: " + ourAnimals[0][2]);
-var g = ourAnimals.toString();  // Converting an array to String for data storage.
-console.log("First array conversion using toString method: " + g);
-var h = g.split(','); // Converting String to an array for Array processing.
-console.log(h);
-var i = ourAnimals.join(',');  // Alternative Conversion of an array to String for data storage.
-console.log("Second array conversion to String using join method: " + i);
-
-// Using 'for' loop to print multi-dimensional array data.
-for (var x in ourAnimals){
-    console.log(ourAnimals[x]);
+window.onscroll = function(){
+    console.log('scroll');
 }
-
-// JavaScript object within Array.
-var myArray = [
-    {
-        name1: "Lawrence",
-        name2: "King",
-        name3: "David"
-    },
-    {
-        name1: "Tina",
-        name2: "Dave",
-        name3: "John"
-
-    }
-]
-
-console.log(myArray[0].name2);
+*/

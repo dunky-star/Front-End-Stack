dddedb5539bfeebf304405709d40fa6c8b6bb231
dunky-star <dukygeoffrey39@gmail.com>
2022-12-05 19:16:01 +0300

    Angular declaration array, import array and export array.

--- AngularDev/Angular-Simplilearn/my-shiny-new-angular-app/src/app/app.component.ts
+++ AngularDev/Angular-Simplilearn/my-shiny-new-angular-app/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom // Doesn't allow the golbal style to leak inside the component.
 })
 export class AppComponent {
   title = 'my-shiny-new-angular-app';

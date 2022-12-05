7901c4bd616cc5667de3f801cf4b6d0980afa29b
dunky-star <dukygeoffrey39@gmail.com>
2022-12-05 13:16:09 +0300

    Creating Angular decorators and modules

---
+++ AngularDev/Angular-Simplilearn/my-shiny-new-angular-app/src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { HeaderComponent } from './header/header.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }

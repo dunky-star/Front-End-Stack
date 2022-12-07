14d8233175e439995a8d8d3ad20d6e13b3287343
dunky-star <dukygeoffrey39@gmail.com>
2022-12-07 16:58:24 +0300

    Implementing Angular routing

--- AngularDev/Angular-Simplilearn/my-shiny-new-angular-app/src/app/app-routing.module.ts
+++ AngularDev/Angular-Simplilearn/my-shiny-new-angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { BlogComponent } from './blog/blog.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Home', component:  HomeComponent },
+  { path: 'About', component:  AboutComponent },
+  { path: 'Blog', component:  BlogComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

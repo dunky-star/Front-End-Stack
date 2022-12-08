4ffc8400a00dba793e740ac46e6d3d8db1fc82d4
dunky-star <dukygeoffrey39@gmail.com>
2022-12-08 09:33:36 +0300

    Implementing Angular Directives

--- AngularDev/Angular-Simplilearn/my-shiny-new-angular-app/src/app/app.module.ts
+++ AngularDev/Angular-Simplilearn/my-shiny-new-angular-app/src/app/app.module.ts
@@ -5,12 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TransformDirective } from './transform.directive';
-import { NoteListComponent } from './components/note-list/note-list.component';
-import { NoteListItemComponent } from './components/note-list-item/note-list-item.component';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { BlogComponent } from './components/blog/blog.component';
-import { HeaderComponent } from './components/header/header.component';
+import { NoteListComponent } from './note-list/note-list.component';
+import { NoteListItemComponent } from './note-list-item/note-list-item.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { HeaderComponent } from './header/header.component';
 import { BuiltInStrDirComponent } from './components/built-in-str-dir/built-in-str-dir.component';
 
 

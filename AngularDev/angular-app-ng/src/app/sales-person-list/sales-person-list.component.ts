1e33bfbca0b16d1db2ab74d96cbdfc6c9c21b523
dunky-star <dukygeoffrey39@gmail.com>
2022-12-15 18:40:37 +0300

    Behind the scene of an Angular project

--- AngularDev/angular-app-ng/src/app/sales-person-list/sales-person-list.component.ts
+++ AngularDev/angular-app-ng/src/app/sales-person-list/sales-person-list.component.ts
@@ -8,7 +8,7 @@ import { SalesPerson } from './sales-person';
 })
 export class SalesPersonListComponent implements OnInit{
   // Creates an array of object.
-  SalesPersonList: SalesPerson[] = [
+  salesPersonList: SalesPerson[] = [
     new SalesPerson("Dunky", "Geoffrey", "geoffrey@mail.com", 10000),
     new SalesPerson("Claire", "Murhy", "claire@mail.com", 60000),
     new SalesPerson("Hector", "Kaligs", "hector@mail.com", 80000),

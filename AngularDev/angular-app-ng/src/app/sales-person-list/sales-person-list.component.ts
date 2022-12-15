c459ab48ce62e3f3c1f078561ec9712c966980ac
dunky-star <dukygeoffrey39@gmail.com>
2022-12-15 18:34:17 +0300

    Behind the scene of an Angular project

--- AngularDev/angular-app-ng/src/app/sales-person-list/sales-person-list.component.ts
+++ AngularDev/angular-app-ng/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
   templateUrl: './sales-person-list.component.html',
   styleUrls: ['./sales-person-list.component.css']
 })
-export class SalesPersonListComponent {
+export class SalesPersonListComponent implements OnInit{
+  // Creates an array of object.
+  SalesPersonList: SalesPerson[] = [
+    new SalesPerson("Dunky", "Geoffrey", "geoffrey@mail.com", 10000),
+    new SalesPerson("Claire", "Murhy", "claire@mail.com", 60000),
+    new SalesPerson("Hector", "Kaligs", "hector@mail.com", 80000),
+    new SalesPerson("Mai", "Kay", "kay@mail.com", 50000)
+
+  ]
+
+  constructor(){}
+
+  ngOnInit(){
+
+  }
 
 }

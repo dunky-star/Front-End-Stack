d5009fd4ddf726306b5494036f7256d432937528
dunky-star <dunkygeoffrey39@gmail.com>
2024-02-17 21:12:07 -0600

    Java Script Functions, objects, Arrays and loops

--- js-fundamental-2/js/custom.js
+++ js-fundamental-2/js/custom.js
@@ -53,3 +53,53 @@ switch (operation) {
 
 }
 
+
+// Function calling other functions
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;  // Arrow function
+
+const scoreDolphins = calcAverage(44, 23, 17);
+const scoreKoalas = calcAverage(65, 54, 49);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log(`Dolphin wins🏆 (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log("No team wins...");
+    }
+}
+
+
+checkWinner(scoreDolphins, scoreKoalas); // Invoke Function
+
+
+
+/*
+* Array: (Data Structure)
+*/
+const friends = ["Duncan", "Peter", "Charles", "Bob"];
+
+friends[2] = "Jay";
+
+const yrs = new Array(1986, 1987, 1988, 1989, 1990, 1991);
+
+console.log("Friends array length: " + friends.length);
+console.log("Last element of the friends array: " + friends[friends.length - 1])
+
+const firstName = "Jonas";
+const jonas = [firstName, "Schmedtmann", 2037 - 1988, "teacher", friends];
+console.log("Jonas detail: " + jonas);
+
+
+// Array manipulation
+
+const calcAges = function (birthYear) {
+    return 2024 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const ages = [calcAges(years[0]), calcAges(years[1]), calcAges(years[2]), calcAges(years[years.length - 1])];
+console.log("Ages: " + ages);

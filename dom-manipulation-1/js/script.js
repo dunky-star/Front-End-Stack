a67de7352ad7fbf62e5de81a6d80b1c02c5f7439
dunky-star <dunkygeoffrey39@gmail.com>
2024-02-19 21:39:40 -0600

    Guess my Number

--- dom-manipulation-1/js/script.js
+++ dom-manipulation-1/js/script.js
@@ -3,3 +3,9 @@
 const message = document.querySelector('.message').textContent; // Querying and element by class name.
 
 console.log(message);
+
+document.querySelector('.message').textContent = '✨Correct Number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);

cd9726467927e46acaac3fb068c3bd79b2909f28
dunky-star <dunkygeoffrey39@gmail.com>
2024-03-12 14:00:52 -0500

    Express routing

---
+++ WAP/W3D2/express-routes/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/add-user', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'views', 'add-user.html'));
+});
+app.post('/add-user', (req, res, next) => {
+  res.redirect('/');
+});
+
+app.get('/login', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
+
+app.post('/login', (req, res, next) => {
+  res.redirect('/add-user');
+});
+
+app.use((req, res, next) => {
+  res.sendFile(path.join('views', '404.html'));
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.toString());
+  res.status(500).send('<h1>Oops! Something went wrong</h1>');
+});
+
+app.listen(3000, () => console.log('Server started and running on port 3000'));

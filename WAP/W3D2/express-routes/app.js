f23f58736510e8c6eeee136a72b89ba19ee9fffe
dunky-star <dunkygeoffrey39@gmail.com>
2024-03-12 14:31:45 -0500

    Express routing

--- WAP/W3D2/express-routes/app.js
+++ WAP/W3D2/express-routes/app.js
@@ -7,18 +7,31 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get('/add-user', (req, res, next) => {
-  res.sendFile(path.join(__dirname, 'views', 'add-user.html'));
+  res.sendFile(path.join(__dirname, './', 'views', 'add-user.html'));
 });
 app.post('/add-user', (req, res, next) => {
+  res.send({
+    username: req.body.username,
+    password: req.body.password,
+    age: req.body.age,
+  });
   res.redirect('/');
 });
 
 app.get('/login', (req, res, next) => {
-  res.sendFile(path.join(__dirname, 'views', 'login.html'));
+  res.sendFile(path.join(__dirname, './', 'views', 'login.html'));
 });
 
 app.post('/login', (req, res, next) => {
+  res.send({
+    username: req.body.username,
+    password: req.body.password,
+  });
   res.redirect('/add-user');
+});
+
+app.use('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, './', 'views', 'index.html'));
 });
 
 app.use((req, res, next) => {
